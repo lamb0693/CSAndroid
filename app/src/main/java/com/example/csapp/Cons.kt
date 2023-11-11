ba2ff9a17fead07b6cb556de57a13684387f04b9
lamb0693 <mar189@naver.com>
2023-11-11 21:47:02 +0900

    change for linux  upload folder and save path

--- app/src/main/java/com/example/csapp/Cons.kt
+++ app/src/main/java/com/example/csapp/Cons.kt
@@ -3,6 +3,6 @@ package com.example.csapp
 object Cons {
 //    const val BASE_URL = "http://10.100.203.29:8080"
 //    const val SOCKETIO_URL = "http://10.100.203.29:3000/counsel"
-    const val BASE_URL = "http://192.168.200.197:8080"
-    const val SOCKETIO_URL = "http://192.168.200.197:3000/counsel"
+    const val BASE_URL = "http://15.164.61.120:8080"
+    const val SOCKETIO_URL = "http://15.164.61.120:3002/counsel"
 }
